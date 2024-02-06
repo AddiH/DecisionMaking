a4f240efc6c97765b4007d0b4b6322e24c8eba5c
AddiH <astrid.elmann@gmail.com>
2024-02-06 11:48:01 +0100

    notes

--- utils/PVL.R
+++ utils/PVL.R
@@ -1,23 +1,12 @@
 PVL <- function(payoff,ntrials,w,A,a,theta) {
   
   # arrays to populate for simulation
-  x <- array(NA,c(ntrials))
-  X <- array(NA,c(ntrials))
-  u <- array(NA,c(ntrials))
-  Ev <- array(NA,c(ntrials,4))
-  Ev_update <- array(NA,c(ntrials,4))
-  exp_p <- array(NA,c(ntrials,4))
-  p <- array(NA,c(ntrials,4))
-  
-  # free parameters - turn back on when constructing
-  #w <- 2
-  #A <- .5
-  #a <- .1
-  #theta <- 3
-  #--- plot prospect theory function
-  #x <- seq(1,100,1)
-  #y <- x^A
-  #plot(x,y)
+  x <- array(NA,c(ntrials)) # chosen deck (A, B, C or D)
+  X <- array(NA,c(ntrials)) # the payoff for the trial
+  u <- array(NA,c(ntrials)) # the utility of the payoff
+  Ev <- array(NA,c(ntrials,4)) # expected value of each deck
+  exp_p <- array(NA,c(ntrials,4)) # exponential of expected values
+  p <- array(NA,c(ntrials,4)) # probability of choosing each option
   
   x[1] <- rcat(1,c(.25,.25,.25,.25)) # assigning a "flat" probability structure to the first choice (i.e. random choice between the four decks)
   
@@ -27,41 +16,37 @@ PVL <- function(payoff,ntrials,w,A,a,theta) {
   
   for (t in 2:ntrials) {
     
-    u[t] <- ifelse(X[t-1]<0,-w*abs(X[t-1])^A,X[t-1]^A)
     
     for (d in 1:4) {
       
-      Ev_update[t,d] <- Ev[t-1,d] + (a * (u[t] - Ev[t-1,d]))
+      if (x[t-1]==d){ # if the current deck was chosen last trial
+        
+        u[t] <- ifelse(X[t-1]<0,-w*abs(X[t-1])^A,X[t-1]^A) # calculating subjective utiliy
+        Ev[t,d] <- Ev[t-1,d] + (a * (u[t] - Ev[t-1,d])) # set the updated EV
       
-      Ev[t,d] <- ifelse(x[t-1]==d,Ev_update[t,d],Ev[t-1,d])
+        } else { # else
+        Ev[t,d] <- Ev[t-1,d] # copy the previous EV
+      }
       
-      exp_p[t,d] <- exp(theta*Ev[t,d])
+      exp_p[t,d] <- exp(theta*Ev[t,d]) # probability of choosing deck
       
     }
     
     for (d in 1:4) {
-      p[t,d] <- exp_p[t,d]/sum(exp_p[t,])
+      p[t,d] <- exp_p[t,d]/sum(exp_p[t,]) # softmax
     }
     
-    x[t] <- rcat(1,p[t,])
+    x[t] <- rcat(1,p[t,])  # find the chosen deck
     
-    X[t] <- payoff[t,x[t]]
+    X[t] <- payoff[t,x[t]] # get the corresponding payoff
     
   }
   
-  result <- list(x=x,
-                 X=X,
-                 Ev=Ev)
+  result <- list(x=x,   # chosen deck
+                 X=X,   # corresponding payoff
+                 Ev=Ev) # Expected utility
   
   return(result)
-  
-  
-  #turn back on when building
-  #par(mfrow=c(2,2))
-  #plot(Ev[,1])
-  #plot(Ev[,2])
-  #plot(Ev[,3])
-  #plot(Ev[,4])
-  #plot(x)
+
   
 }
